d05f381152f826cded38a91c73d93d87142f56c4
Jose Ruiloba <jose_ruiloba@outlook.com>
2019-01-24 17:34:05 -0700

    Final

---
+++ FuzzBuzz/FuzzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FuzzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Answer = string.Empty;
+
+            for (int i = 1; i < 101; i++)
+            {
+                Answer = string.Empty;
+                if (i % 3==0)
+                {
+                    Answer = "Fuzz";
+                }
+                if (i % 5 == 0)
+                {
+                    Answer = "Buzz";
+                }
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Answer = "FuzzBuzz";
+                }
+                Console.WriteLine(i+" " + Answer);
+            }
+            Console.ReadLine();  
+        }
+    }
+}
